30c1efac32bd850cb943ff8d425d5572a4a4ee22
ahaan7 <82714306+ahaan7@users.noreply.github.com>
2021-06-07 20:19:31 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+const Engine=Matter.Engine
+const World=Matter.World
+const Bodies=Matter.Bodies
+var bg 
+var maxsnow=100
+var snow=[]
+var rand
+function preload(){
+  bg=loadImage("snow2.jpg")
+}
+  
+
+function setup() {
+  createCanvas(800,400);
+  engine=Engine.create()
+  world=engine.world
+
+}
+
+function draw() {
+  background(bg); 
+  Engine.update(engine) 
+  rand=Math.round(random(1,4))
+  if(frameCount%5===0){
+    snow.push(new Snow(random(0,800),30,30))
+ 
+  }
+  for(var j=0;j<snow.length;j++){
+  snow[j].display()
+  }
+  drawSprites();
+}
